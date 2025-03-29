aa43ecd663f132e419f510aa38024076b993e7fa
Disitha <disithar@gmail.com>
2025-03-30 00:37:07 +0530

    Add REST endpoints to MyapplicationController for greeting and age retrieval

--- day03/demo/src/main/java/com/example/demo/controller/MyapplicationController.java
+++ day03/demo/src/main/java/com/example/demo/controller/MyapplicationController.java
@@ -1,5 +1,26 @@
 package com.example.demo.controller;
 
-public class MyapplicationController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/app")
+public class MyapplicationController {
+	
+	@GetMapping("/msg")
+	public String mymsg() {
+		return "Hello Springboot";
+	}
+	
+	@GetMapping("/name")
+	public String myName() {
+		return "Hello Iam Disith";
+	}
+	
+	@GetMapping("/age/{ag}")
+	public String myAge(@PathVariable("ag") int age) {
+		return "My age is "+age;
+	}
 }
