2f2d7b4c47c292ec3501d2ca8a1ab9e1f065949e
Disitha <disithar@gmail.com>
2025-03-30 19:54:50 +0530

    Add multiple Student instances and a method to retrieve a single student in MyapplicationController

--- day03/demo/src/main/java/com/example/demo/controller/MyapplicationController.java
+++ day03/demo/src/main/java/com/example/demo/controller/MyapplicationController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import java.awt.List;
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +13,12 @@ import com.example.demo.model.Student;
 @RequestMapping("/app")
 public class MyapplicationController {
 	
-	Student Bob = new Student("2020/ICT/23","Bob",23,"IT",1.0);
+	Student Bob = new Student("2020/ICT/113","Bob",23,"IT",1.0);
+	Student Naml = new Student("2020/ICT/12","Naml",24,"AMC",2.0);
+	Student Kaml = new Student("2020/ICT/13","Kaml",20,"ASB",3.0);
+	Student Kasun = new Student("2020/ICT/110","Kasun",21,"TECH",1.4);
+	
+	java.util.List<Student> students = new ArrayList<Student>();
 	
 	@GetMapping("/msg")
 	public String mymsg() {
@@ -26,4 +34,14 @@ public class MyapplicationController {
 	public String myAge(@PathVariable("ag") int age) {
 		return "My age is "+age;
 	}
+	
+	//method to return only one student
+	@GetMapping("/Bobstudent")
+	public Student getStudent() {
+		return Bob;
+	}
+	
+	//return all the students using proper datastructure
+	
+	
 }
