66b02a5128ab0e7bc505434bf92bc4fc0827162a
Disitha Ranasinghe <120271466+Disitha395ra@users.noreply.github.com>
2025-03-31 17:49:23 +0530

    Update MyapplicationController.java
    re doing CRUD operations

--- day03/demo/src/main/java/com/example/demo/controller/MyapplicationController.java
+++ day03/demo/src/main/java/com/example/demo/controller/MyapplicationController.java
@@ -1,65 +1,57 @@
 package com.example.demo.controller;
 
-import java.awt.List;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.stream.Collector;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.demo.DemoApplication;
+
 import com.example.demo.model.Student;
+
+
 
 @RestController
 @RequestMapping("/app")
-public class MyapplicationController {
-
-    private final DemoApplication demoApplication;
+public class Myapplicationcontroller {
 	
-	Student Bob = new Student("2020/ICT/113","Bob",23,"IT",1.0);
-	Student Naml = new Student("2020/ICT/12","Naml",24,"AMC",2.0);
-	Student Kaml = new Student("2020/ICT/13","Kaml",20,"ASB",3.0);
-	Student Kasun = new Student("2020/ICT/110","Kasun",21,"TECH",1.4);
+	Student Bob = new Student("2020-ICT-113", "Bob", "IT", 23,2.5);
+	Student Bib = new Student("2020-ICT-13", "Bib", "IT", 22,2.8);
+	Student Lho = new Student("2020-ICT-103", "Lho", "IT", 22,2.2);
+	Student Rcs = new Student("2020-ICT-03", "Rcs", "IT", 21,2.3);
 	
-	java.util.List<Student> students = new ArrayList<Student>();
-
-        public Myapplicationcontroller() {
+	List<Student> students = new ArrayList<Student>();
+	
+	public Myapplicationcontroller() {
 		students.add(Bob);
-		students.add(Naml);
-		students.add(Kaml);
-		students.add(Kasun);
-	}
-	@GetMapping("/msg")
-	public String mymsg() {
-		return "Hello Springboot";
+		students.add(Bib);
+		students.add(Lho);
+		students.add(Rcs);
 	}
 	
-	@GetMapping("/name")
-	public String myName() {
-		return "Hello Iam Disith";
+	@GetMapping("/mymsg")
+	public String myMsg() {
+		return "Hii Spring boot";
 	}
 	
-	@GetMapping("/age/{ag}")
-	public String myAge(@PathVariable("ag") int age) {
-		return "My age is "+age;
-	}
-	
-	//method to return only one student
-	@GetMapping("/Bobstudent")
+	@GetMapping("/student")
 	public Student getStudent() {
 		return Bob;
 	}
-
+	
 	//print all student
 	@GetMapping("/allstudents")
 	public List<Student>getAllStudent(){
 		return students;
 	}
 	
-	//return all the students using proper datastructure
+	//find student using regNo
 	@GetMapping("/findbyregno/{regNo}")
 	public Student findstudentbyregNo(@PathVariable("regNo") String regNo){
 		for(Student student:students) {
@@ -70,7 +62,7 @@ public class MyapplicationController {
 		return null;
 	}
 	
-	//find the students whoose age is between 20 and 23
+	//find student by age range
 	@GetMapping("/findbyagerange/{min}/{max}")
 	public List<Student> findstudentbyagerange(@PathVariable("min") int min, @PathVariable("max") int max) {
 		List<Student> filteredStudents = new ArrayList<>();
@@ -83,7 +75,6 @@ public class MyapplicationController {
 		return filteredStudents;
 	}
 	
-	//sorts the students by their GPA
 	//sort student by their gpa
 	@GetMapping("/students/sortbygpa")
 	public List<Student> sortbygpa(){
@@ -92,5 +83,37 @@ public class MyapplicationController {
 				.collect(Collectors.toList());
 	}
 	
-	//create CRUD opperations for students
+	//CRUD (Create, Read, Update, Delete) operations
+	//create student
+	@PostMapping("/student/addstudent")
+	public String addStudent(@RequestBody Student student) {
+		students.add(student);
+		return "Student add successfully";
+	}
+	
+	//Read all students
+	@GetMapping("/students/allstudents")
+	public List<Student> getAll(){
+		return students;
+	}
+	
+	//update student
+	@PutMapping("studentdetails/{regNo}")
+	public String updateStudent(@PathVariable String regNo, @RequestBody Student updatedStudent) {
+		for(Student student:students) {
+			if(student.getRegNo().equals(regNo)) {
+				student.setName(updatedStudent.getName());
+				student.setCourse(updatedStudent.getCourse());
+				student.setAge(updatedStudent.getAge());
+				student.setGpa(updatedStudent.getGpa());
+				return "Student update successfully";
+			}
+		}
+		return "Student not found";
+	}
+	
+	//Delete Student
+	
+	
+
 }
