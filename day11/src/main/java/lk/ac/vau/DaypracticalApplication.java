2e15da5dafff7ea3aeef910c2bec7d69a9bec75d
Disitha Ranasinghe <120271466+Disitha395ra@users.noreply.github.com>
2025-06-06 03:52:29 +0000

    Initialize Spring Boot application with main entry point

---
+++ day11/src/main/java/lk/ac/vau/DaypracticalApplication.java
@@ -0,0 +1,13 @@
+package lk.ac.vau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaypracticalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaypracticalApplication.class, args);
+	}
+
+}
