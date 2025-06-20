f7ba99e82d38bbe32946072dba4655eab5eaad17
Disitha <disithar@gmail.com>
2025-06-20 07:30:21 +0530

    Add initial project structure and configuration for ICA2again application

---
+++ ICA2/ICA2again/src/main/java/com/example/demo/Ica2againApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ica2againApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ica2againApplication.class, args);
+	}
+
+}
